5e29c84dc6445c299e24f6634192a84113af68c0
Yami <106425085+MaheryRandrianirina@users.noreply.github.com>
2026-02-02 10:31:34 +0100

    Fix marginTop style in Banner component
    Updated the Banner component to change marginTop style.

--- components/sections/banner.tsx
+++ components/sections/banner.tsx
@@ -4,7 +4,7 @@ import { PrimaryButton } from "../buttons";
 
 
 export const Banner = forwardRef((props, ref: LegacyRef<HTMLElement>)=>{
-    return <section ref={ref} id="banner" className="bg-dark d-md-flex justify-content-between align-items-center" style={{marginBottom: "48px", paddingTop: "60px"}}>
+    return <section ref={ref} id="banner" className="bg-dark d-md-flex justify-content-between align-items-center" style={{marginBottom: "48px", marginTop: "60px"}}>
         <div className="ms-2">
             <div style={{width: "350px"}} className="mx-sm-auto my-sm-0 intro">
                 <h1 className="text-light" style={{marginBottom: "24px"}}>Mahery Randrianirina - Développeur Fullstack PHP & Javascript</h1>
