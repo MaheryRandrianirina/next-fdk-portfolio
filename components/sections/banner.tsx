446ab5c7e84b07abb3eaa2f87f21d6daf031ecbd
Yami <106425085+MaheryRandrianirina@users.noreply.github.com>
2026-02-02 10:09:51 +0100

    Chore: modify banner description

--- components/sections/banner.tsx
+++ components/sections/banner.tsx
@@ -7,13 +7,13 @@ export const Banner = forwardRef((props, ref: LegacyRef<HTMLElement>)=>{
     return <section ref={ref} id="banner" className="bg-dark d-md-flex justify-content-between align-items-center" style={{marginBottom: "48px", paddingTop: "60px"}}>
         <div className="ms-2">
             <div style={{width: "350px"}} className="mx-sm-auto my-sm-0 intro">
-                <h1 className="text-light" style={{marginBottom: "24px"}}>FDK - Fast Data Keys</h1>
+                <h1 className="text-light" style={{marginBottom: "24px"}}>Mahery Randrianirina - Développeur Fullstack PHP & Javascript</h1>
                 <p className="text-light" style={{marginBottom: "24px"}}>
                     Du code propre, des designs captivants, des solutions innovantes.
-                    <span className="text-bold">Créons ensemble des sites web qui marquent les esprits.</span>
+                    <span className="text-bold">Créons ensemble des sites et applications web qui marquent les esprits.</span>
                 </p>
                 <PrimaryButton className="cta">
-                    <a href="#contact" className="text-light text-decoration-none">Nous contacter</a>
+                    <a href="#contact" className="text-light text-decoration-none">Me contacter</a>
                 </PrimaryButton> 
             </div>           
         </div>
